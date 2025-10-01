4f439719d9e304ad84dbe0de81de8417e7a9385e
jknanda78 <j_k_nanda@hotmail.com>
2025-09-30 21:46:25 -0700

    initial dev work

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import compression from "compression";
+import path from "path";
+import zlib from "node:zlib";
+
+const app = express();
+const PORT = 3000;
+const jsonData = require("./data.json");
+
+// Enable compression for all responses
+/*
+app.use(
+  compression({
+    threshold: 0,
+    filter: (req, res) => compression.filter(req, res),
+  })
+);
+*/
+
+app.get("/bigData", (req, res) => {
+  try {
+    const compressedData = zlib.gzipSync(JSON.stringify(jsonData));
+    res.set("Content-Encoding", "gzip");
+    res.send(compressedData);
+  } catch (err) {
+    res.status(500).send("Error compressing data");
+  }
+});
+
+app.get("/", (req, res, next) => {
+  const _retfile = path.join(__dirname, "test.html");
+  res.sendFile(_retfile);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
